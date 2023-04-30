0356e6e62274574e097eb70e8cd17801866e0fc5
SX-9 <sx-91@outlook.com>
2023-04-30 14:30:04 +0700

    css fixes

--- js/cmds/socials.js
+++ js/cmds/socials.js
@@ -1,7 +1,7 @@
 export default function(output) {
     output.innerHTML += `\
 <span data-color="white">Github: <a href="https://github.com/SX-9">@SX-9</a></span>
-<span data-color="blue">Discord: <a href="https://discord.st/sx-aircraft">sx9dev#1151</a></span>
+<span data-color="MediumSlateBlue">Discord: <a href="https://discord.st/sx-aircraft">sx9dev#1151</a></span>
 <span data-color="cyan">Twitter: <a href="https://twitter.com/SX_Discord">SX_Discord</a></span>
 <span data-color="yellow">Email: <a href="mailto:hello@mail.sx9.is-a.dev">hello@mail.sx9.is-a.dev</a></span>
 `;
