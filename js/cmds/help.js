0187629c99ad8e132552a89a74fdca80737d893b
SX-9 <sx-91@outlook.com>
2023-04-30 13:27:14 +0700

    spell fix

--- js/cmds/help.js
+++ js/cmds/help.js
@@ -12,7 +12,7 @@ Extra Commands For Fun:
     <span data-color="lime">history</span> <span data-color="grey">-</span> <span data-color="yellow">Prints Your Command History.</span>
     <span data-color="lime">whoami</span> <span data-color="grey">-</span> <span data-color="yellow">What's Your Name?</span>
     <span data-color="lime">hostname</span> <span data-color="grey">-</span> <span data-color="yellow">What Is This?</span>
-    <span data-color="lime">clear</span> <span data-color="grey">-</span> <span data-color="yellow">Clears The Terminal</span>
+    <span data-color="lime">clear</span> <span data-color="grey">-</span> <span data-color="yellow">Clears The Terminal.</span>
     <span data-color="lime">echo [text]</span> <span data-color="grey">-</span> <span data-color="yellow">Prints [text].</span>
     <span data-color="lime">eval [code]</span> <span data-color="grey">-</span> <span data-color="yellow">Eval [code] As JS.</span>
 `;
