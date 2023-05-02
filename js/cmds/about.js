14778b66912451cdb63196e324b4c11521ab570e
SX Spy Agent <sx-91@outlook.com>
2023-05-02 22:15:42 +0700

    update about

--- js/cmds/about.js
+++ js/cmds/about.js
@@ -1,5 +1,5 @@
 export default function(output) {
     output.innerHTML += `\
-<p>I'm Satria, a web developer from Indonesia. I started programming in 2021 and initially wanted to learn Python scripting through a tutorial series on YouTube. However, I quit because the series was taking too long to finish. Later, I made my own Discord server and became interested in making Discord bots. Since Discord API updates frequently, it became challenging to learn the new features, so I switched to web development. I started with HTML, CSS, and JavaScript, and now I consider myself an intermediate web developer.</p>\
+Hey there, I'm Satria! I'm a self-taught web developer with a passion for coding and biking. I've been coding since 2021, with experience in Node.js, web development, and Linux. My interest in low-level systems, circuitry, and machine learning keeps me engaged and curious. I'm fluent in HTML, CSS, and JavaScript, and I enjoy learning new languages and frameworks. As an introverted student, I enjoy spending time improving my skills and working on personal projects. If you want to connect with another coding enthusiast, don't hesitate to reach out!\
 `;
 }
