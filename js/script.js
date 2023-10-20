68dbd540291b5d969a842ef79b432062429e0021
sx9dev <sx-91@outlook.com>
2023-10-20 15:48:40 +0700

    remove fix

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-if (location.host.indexOf("localhost") < 0 && location.protocol.toLowerCase() !== "https:") {
-    const url = `https://${location.host}`;
-    location.replace(url);
-}
+
+// this code doesnt work
+
+//if (location.host.indexOf("localhost") < 0 && location.protocol.toLowerCase() !== "https:") {
+//    const url = `https://${location.host}`;
+//    location.replace(url);
+//}
+
+// end broken code
 
 import "./color.js";
 import "./shell.js";
