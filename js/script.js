766f1a6309e7441640634f6c737b1fdcaaa4996e
sx9dev <sx-91@outlook.com>
2023-10-20 15:40:27 +0700

    fix reload on http

--- js/script.js
+++ js/script.js
@@ -1,7 +1,4 @@
-if (location.protocol !== "https" && !location.hostname.startsWith('localhost')) {
-    location.protocol = "https";
-    location.reload();
-}
+if (location.protocol !== "https" && !location.hostname.startsWith('localhost')) location.protocol = "https";
 
 import "./color.js";
 import "./shell.js";
