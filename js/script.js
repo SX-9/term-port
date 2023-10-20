b9156e2b797ac17d56c0a9026dba82e50d5722df
Satria <satriakrach@gmail.com>
2023-10-20 15:36:28 +0700

    auto redirect to https

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
+if (location.protocol !== "https" && !location.hostname.startsWith('localhost')) {
+    location.protocol = "https";
+    location.reload();
+}
+
 import "./color.js";
 import "./shell.js";
